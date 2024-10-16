cbfeb35e62813030ddcd5fe61c6747bd02278382
Biki101 <thakurbiki52@gmail.com>
2024-10-16 20:56:39 +0545

    added intro section

---
+++ src/app/components/Header/Intro/intro.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import introChef from "../../../assets/intro-chef.png";
+import chefWithDish from "../../../assets/chef-with-dish-intro.jpg";
+
+export default function Intro() {
+  return (
+    <div className="outer-container">
+      <div className="relative">
+        <div className="absolute top-[200px] w-[600px] bg-white border-[2px] border-[var(--gold-color)] p-[50px] left-0">
+          <div className="text-[36px] text-[var(--gold-color)]">
+            Something New
+          </div>
+          <div className="text-[25px]">An Extra Ordinary Experience</div>
+          <div className="mt-[10px] text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            labore quidem, officiis eos animi iure, atque sunt fugiat magni. et
+            sit eveniet, ab porro commodi perferendis non!
+          </div>
+        </div>
+      </div>
+      <div className="h-[700px] flex gap-[30px] justify-end items-center">
+        <Image
+          src={introChef}
+          width={350}
+          height={560}
+          className="w-[350px] h-[560px]"
+          alt="intro-chef"
+        />
+        <Image
+          src={chefWithDish}
+          width={350}
+          height={560}
+          alt="intro-chef"
+          className="w-[350px] h-[560px]"
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,39 @@ import Image from "next/image";
 import Header from "./components/Header/header";
 import heroImage from "./assets/hero.jpg";
 import WelcomeMessage from "./components/Header/WelcomeMessahe/welcome-message";
+import Intro from "./components/Header/Intro/intro";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between relative h-[100vh]">
-      {/* hero Image */}
-      <Image
-        src={heroImage}
-        width={1920}
-        height={180}
-        className="h-[100vh] w-full absolute top-0 left-0 z-[0]"
-        alt="hero image"
-      />
-      {/* hero Image End */}
-      <Header />
-      <WelcomeMessage />
-      <div className="z-[1] slide-top mx-auto">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="30"
-          viewBox="0 0 24 24"
-          width="30"
-        >
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path
-            d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"
-            fill="white"
-          />
-        </svg>
+    <div>
+      <div className="flex flex-col justify-between relative h-[100vh]">
+        {/* hero Image */}
+        <Image
+          src={heroImage}
+          width={1920}
+          height={180}
+          className="h-[100vh] w-full absolute top-0 left-0 z-[0]"
+          alt="hero image"
+        />
+        {/* hero Image End */}
+        <Header />
+        <WelcomeMessage />
+        <div className="z-[1] slide-top mx-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30"
+            viewBox="0 0 24 24"
+            width="30"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"
+              fill="white"
+            />
+          </svg>
+        </div>
       </div>
+      <Intro />
     </div>
   );
 }
